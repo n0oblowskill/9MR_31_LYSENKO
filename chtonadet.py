7b55319f11e4b9c852e09600813fb0269b31ccc5
n0oblowskill <aptemohasss@gmail.com>
2026-02-10 20:04:47 +0300

    v 2.0

--- chtonadet.py
+++ chtonadet.py
@@ -1,1 +1,28 @@
-test 
+
+
+print("Что надеть ?")
+temp = int(input("Введите температуру: "))
+rain = int(input("Есть ли осадки? (1/2) 1 - да 2 - нет: "))
+
+
+if rain == 1 and 30 > temp > 20 :
+    print("футболку, шорты, дождевик")
+
+if rain == 2 and 30 > temp > 20 :
+    print("футболку, шорты")
+if 20 > temp > 0  :
+    level_rain = int(input("Осадки сильные? (1/2) 1 - да 2 - нет: "))
+
+    if 20 > temp > 0 and rain == 1 and level_rain == 1:
+        print("пальто, резиновые сапоги, зонт")
+
+    if 20 > temp > 0 and rain == 1 and level_rain == 2:
+        print("пальто, дождевик")
+
+if temp < 0:
+    print("пуховик")
+
+if 20 > temp > 0  and rain == 2 :
+    print("пальто")
+
+print("Хорошего дня :)")
